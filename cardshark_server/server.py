b95fbd63d9b6fa1bca386504ba4f57e410e19b50
Daniel Jones <danielmarkjones9@gmail.com>
2024-01-21 21:14:31 -0500

    test random number server request

--- cardshark_server/server.py
+++ cardshark_server/server.py
@@ -1,8 +1,9 @@
 from flask import Flask
 import random
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def random_number():
-    return str(random.randint(0,15))
+    return json.dumps({"value":str(random.randint(0,15))})
