fd82fe75c114619c79f85e651b4e5d40e4323115
danbot <danielmarkjones9@gmail.com>
2025-02-08 11:50:18 -0500

    Closes #6

--- utils.js
+++ utils.js
@@ -1,8 +1,8 @@
-function import_html(html, loc){
-    fetch(html)
+function import_html(path_to_html, id_to_insert){
+    fetch(path_to_html)
         .then(response => response.text())
         .then(data => {
-            document.getElementById(loc).innerHTML = data;
+            document.getElementById(id_to_insert).innerHTML = data;
         });
 }
 export default import_html
