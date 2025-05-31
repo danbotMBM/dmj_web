7f7cc5c5c0b8ff383ea48ee3d47db0109912e35f
danbot <danielmarkjones9@gmail.com>
2025-05-30 21:53:28 -0400

    added wikipedia like zoom function for desktop; working on moble on photos page

--- lazyload.js
+++ lazyload.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
+            console.log(entry.target)
             if (entry.isIntersecting) {
                 // Get the image and update the src attribute
                 const img = entry.target;
@@ -17,10 +18,70 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 img.src = src;
                 observer.unobserve(img); // Stop observing after loading
+                set_up_zoomable(img);
             }
         });
     }, { rootMargin: "200px" });
 
     // Start observing each image
     images.forEach(img => observer.observe(img));
+});
+
+const viewer = document.getElementById('image-viewer');
+const fullImage = document.getElementById('full-image');
+const closeBtn = document.getElementById('close-btn');
+
+function set_up_zoomable(image){
+    let src = image.getAttribute('src')
+    image.dataset.full = src.replace('low', 'high')
+    image.addEventListener('click', () => {
+        console.log('click')
+        const fullSrc = image.dataset.full;
+        fullImage.src = fullSrc;
+        viewer.style.display = 'flex';
+        zoomed = false;
+        fullImage.style.transform = 'scale(1)';
+        fullImage.style.cursor = 'zoom-in';
+        fullImage.style.transformOrigin = 'center center';
+    });
+}
+
+let zoomed = false;
+
+
+closeBtn.addEventListener('click', closeViewer);
+viewer.addEventListener('click', (e) => {
+    if (e.target === viewer) closeViewer();
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') closeViewer();
+});
+
+function closeViewer() {
+    viewer.style.display = 'none';
+    zoomed = false;
+    fullImage.style.transform = 'scale(1)';
+    fullImage.style.cursor = 'zoom-in';
+    fullImage.style.transformOrigin = 'center center';
+}
+
+fullImage.addEventListener('click', (e) => {
+    const rect = fullImage.getBoundingClientRect();
+    const offsetX = e.clientX - rect.left;
+    const offsetY = e.clientY - rect.top;
+    const originX = (offsetX / rect.width) * 100;
+    const originY = (offsetY / rect.height) * 100;
+
+    if (!zoomed) {
+    fullImage.style.transformOrigin = `${originX}% ${originY}%`;
+    fullImage.style.transform = 'scale(3)';
+    fullImage.style.cursor = 'zoom-out';
+    } else {
+    fullImage.style.transformOrigin = `${originX}% ${originY}%`;
+    fullImage.style.transform = 'scale(1)';
+    fullImage.style.cursor = 'zoom-in';
+    }
+
+    zoomed = !zoomed;
 });
